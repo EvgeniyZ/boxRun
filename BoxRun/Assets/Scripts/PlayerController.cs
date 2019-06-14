945bafe4bfc0279801189d9b408bbc974f4bc341
Evgenii Zakharov <eugene.zakharov93@gmail.com>
2019-06-14 23:55:49 +0300

    add playerController script

---
+++ BoxRun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
